9fb9f440a2cbfc6fcd9ff2666927375d5e825cdb
Nicholas Frota <info@nicholasfrota.com>
2015-11-29 20:19:11 -0500

    readme.template

--- gulp/tasks/readme.js
+++ gulp/tasks/readme.js
@@ -3,7 +3,7 @@ $ = require('gulp-load-plugins')(),
 config = require('../config');
 
 gulp.task('readme', function() {
-  return gulp.src(['./INFO.md', './TODO.md'])
+  return gulp.src(['./README.template.md', './TODO.md'])
     .pipe($.concat('README.md'))
     .pipe(gulp.dest('./'));
 });
