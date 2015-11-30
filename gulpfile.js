94217ba0369ea10389d983f2c1c33439607c0ec2
Nicholas Frota <info@nicholasfrota.com>
2015-11-30 18:08:06 -0500

    partials

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,4 @@ requireDir = require('require-dir');
 requireDir('./gulp/tasks');
 
 
-gulp.task('js-concat', function() {
-  return gulp.src([ './app/js/vendors/jquery.js', './app/js/vendors/bootstrap.js', './app/js/vendors/modernizr.js', './app/js/vendors/detectizr.js', './app/js/vendors/chosen.js', './app/js/scripts.js'])
-    .pipe($.concat('main.js'))
-    .pipe(gulp.dest('./dist/js'));
-});
-
 
